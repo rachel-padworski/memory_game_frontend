34826d16355e3f0d99b70aa420432d352d915304
github username <github email address>
2020-10-12 19:21:15 -0600

    adds fetch to backend

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+const endPoint = "http://localhost:3000/api/v1/games"
+
 document.addEventListener('DOMContentLoaded', () => {
-    
-    
-
-
-
-
-
-
-
-
-
-
+    // console.log("DOM loaded")
+    getGame();
 })
+
+function getGame() {
+    fetch("http://localhost:3000/api/v1/games")
+    .then(response => response.json())
+    .then(games => {
+        games.data.forEach(game => {
+            
+        });
+    })
+    
+}
