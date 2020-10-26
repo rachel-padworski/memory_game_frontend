eb53e0c87e1cdd9201a8b387a3214ad6fd69a83b
github username <github email address>
2020-10-25 20:27:00 -0600

    fixes image files

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ let cardsArray = []
 let cardsChosen = []
 let cardsChosenId = []
 let cardsWon = []
-const grid = document.querySelector('#grid-id')
+const grid = document.querySelector('.grid')
 const resultDisplay = document.querySelector('#result')
 cardsArray.sort(() => 0.5 - Math.random())
 
@@ -18,8 +18,9 @@ function getGame() {
         // console.log(games.data[0].attributes.cards)
         games.data[0].attributes.cards.forEach(cards => {
             
-            //     // console.log(cards)
+            // console.log(cards)
             let newCard = new Card(cards)
+            // console.log(newCard)
             cardsArray.push(newCard)
             //     console.log(card)
             //     // cardsArray.push(card)
@@ -28,46 +29,46 @@ function getGame() {
 }
 getGame()
 
-// function createBoard() {
-//     for (let i = 0; i < cardsArray.length; i++) {
-//         let card = document.createElement('img')
-//         card.setAttribute('src', "../src/images/BackOfCard.png")
-//         card.setAttribute('id', i)
-//         card.addEventListener('click', flipCard)
-//         grid.appendChild(card)
-//     }
-// }
-// createBoard()
+function createBoard() {
+    for (let i = 0; i < cardsArray.length; i++) {
+        let card = document.createElement('img')
+        card.setAttribute('src', "../src/images/BackOfCard.png")
+        card.setAttribute('id', i)
+        card.addEventListener('click', flipCard)
+        grid.appendChild(card)
+    }
+}
+createBoard()
 
-// //flip the selected cards
-// function flipCard() {
-//     let cardId = this.getAttribute('id')
-//     cardsChosen.push(cardsArray[cardId].title)
-//     cardsChosen.push(cardId)
-//     this.setAttribute('src', cardsArray[cardId].image_url)
-//     if (cardsChosen.length === 2) {
-//         setTimeout(checkForMatch, 500)
-//     }
-// }
+//flip the selected cards
+function flipCard() {
+    let cardId = this.getAttribute('id')
+    cardsChosen.push(cardsArray[cardId].title)
+    cardsChosen.push(cardId)
+    this.setAttribute('src', cardsArray[cardId].image_url)
+    if (cardsChosen.length === 2) {
+        setTimeout(checkForMatch, 500)
+    }
+}
 
-// //checks for a match
-// function checkForMatch() {
-//     let cards = document.querySelectorAll('img')
-//     const optionOneId = cardsChosenId[0]
-//     const optionTwoId = cardsChosenId[1]
+//checks for a match
+function checkForMatch() {
+    let cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenId[0]
+    const optionTwoId = cardsChosenId[1]
 
-//     if (cardsChosen[0] === cardsChosen[1]) {
-//         alert('You found a match!')
-//         cardsWon.push(cardsChosen)
-//     } else {
-//         cards[optionOneId].setAttribute('src', "../src/images/BackOfCard.png")
-//         cards[optionTwoId].setAttribute('src', "../src/images/BackOfCard.png")        
-//         alert('Sorry, try again')
-//     }
-//     cardsChosen = []
-//     cardsChosenId = []
-//     resultDisplay.textContent = cardsWon.length
-//     if (cardsWon.length === cardsArray.length/2) {
-//         resultDisplay.textContent = 'Congratulations! You found all matches!'
-//     }
-// }
+    if (cardsChosen[0] === cardsChosen[1]) {
+        alert('You found a match!')
+        cardsWon.push(cardsChosen)
+    } else {
+        cards[optionOneId].setAttribute('src', "../src/images/BackOfCard.png")
+        cards[optionTwoId].setAttribute('src', "../src/images/BackOfCard.png")        
+        alert('Sorry, try again')
+    }
+    cardsChosen = []
+    cardsChosenId = []
+    resultDisplay.textContent = cardsWon.length
+    if (cardsWon.length === cardsArray.length/2) {
+        resultDisplay.textContent = 'Congratulations! You found all matches!'
+    }
+}
