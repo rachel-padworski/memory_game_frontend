9cfbdfcf01a49e9e38814bb786816eb84886262a
github username <github email address>
2020-11-02 19:23:06 -0700

    enables player login

--- src/components/loginManager.js
+++ src/components/loginManager.js
@@ -1,80 +1,82 @@
-// class LoginManager {
-//     constructor() {
-//         // this.adapter = new PlayersAdapter(),
-//         this.establishListeners()
-//     }
+class LoginManager {
+    constructor() {
+        // this.adapter = new PlayersAdapter(),
+        this.establishListeners()
+    }
     
-//     establishListeners() {
-//         const loginForm = document.querySelector('#form-container')
-//         loginForm.addEventListener('submit', (e) => this.loginFormHandler(e))
-//     }
+    establishListeners() {
+        const loginForm = document.querySelector('#form-container')
+        loginForm.addEventListener('submit', (e) => this.loginFormHandler(e))
+    }
     
-//     loginFormHandler(e) {
-//         e.preventDefault()
-//         const usernameInput = e.target.querySelector('#input-username').value 
-//         const passwordInput = e.target.querySelector('#login-password').value
-//         this.loginFetch(usernameInput, passwordInput)
-//     }
+    loginFormHandler(e) {
+        e.preventDefault()
+        const usernameInput = e.target.querySelector('#input-username').value 
+        const passwordInput = e.target.querySelector('#login-password').value
+        this.loginFetch(usernameInput, passwordInput)
+    }
 
-//     loginFetch(username, password) {
-//         const bodyData = {player: { username, password }}
+    loginFetch(username, password) {
+        const bodyData = {player: { username, password }}
 
-//         fetch("http://localhost:3000/api/v1/login", {
-//             method: "POST",
-//             headers: {"Content-Type": "application/json"},
-//             body: JSON.stringify(bodyData)
-//         })
-//         .then(response => response.json())
-//         .then(json => {
-//             localStorage.setItem('jwt_token', json.jwt)
-//             this.profileFetch()
-//         })
-//     }
+        fetch("http://localhost:3000/api/v1/login", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(bodyData)
+        })
+        .then(response => response.json())
+        .then(json => {
+            // this.localStorage.setItem('jwt_token', json.jwt)
+            console.log(json)
+            this.profileFetch()
+        })
+    }
 
-//     profileFetch() {
-//         fetch('http://localhost:3000/api/v1/profile', {
-//             method: 'GET',
-//             headers: {
-//                 Authorization: `Bearer ${localStorage.getItem('jwt_token')}`
-//             }
-//         })
-//         .then(response => response.json())
-//         .then(json => {
-//             console.log(json)
-//             //what is happening here
-//             const playerGreeting = document.querySelector('#player-greeting')
-//             playerGreeting.textContent = `Welcome back ${json.player.data.attributes.username}`
-//             // console.log(json.player.data.attributes.username)
-//             // alert(`Welcome back ${json.player.data.attributes.username}`)
-//             // add a feature to count wins to show to player stats
-//             this.closeModal()
-//         })
-//     }
+    profileFetch() {
+        // console.log(this.localStorage.getItem('jwt_token'))
+        // fetch('http://localhost:3000/api/v1/profile', {
+        //     method: 'GET',
+        //     headers: {
+        //         Authorization: `Bearer ${localStorage.getItem('jwt_token')}`
+        //     }
+        // })
+        // .then(response => response.json())
+        // .then(json => {
+        //     console.log(json)
+        //     //what is happening here
+        //     // const playerGreeting = document.querySelector('#player-greeting')
+        //     // playerGreeting.textContent = `Welcome back ${json.player.data.attributes.username}`
+        //     // console.log(json.player.data.attributes.username)
+        //     alert(`Welcome back ${json.player.data.attributes.username}`)
+        //     // add a feature to count wins to show to player stats
+        //     this.closeModal()
+        // })
+        this.closeModal()
+    }
 
-//     closeModal() {
-//         let modal_container = document.getElementById('modal-container')
-//         let submit = document.querySelector('#submit')[0]
-        
-//         //close modal when submit is clicked
-//         submit.addEventListener('submit', () => {
-//             modal_container.style.display = "none";
-//         })
-//     }
+    closeModal() {
+        let modal_container = document.getElementById('modal-container')
+        let submit = document.querySelector('#submit')
+        //close modal when submit is clicked
+        submit.addEventListener('submit', () => {
+            modal_container.style.display = "none";
+        })
+    }
 
 
-    //     // const data = {player: {username}}
-    //     fetch("http://localhost:3000/api/v1/players", {
-    //         method: "POST",
-    //         headers: {"Content-Type": "application/json"},
-    //         body: JSON.stringify({
-    //             username: username
-    //         })
-    //     })
-    //     .then(response => response.json())
-    //     .then(player => {
-    //         // renderPlayerProfile(player) //figure this out
-    //         console.log(player) playerStats()
-    //     })
+        // const data = {player: {username}}
+        // fetch("http://localhost:3000/api/v1/players", {
+        //     method: "POST",
+        //     headers: {"Content-Type": "application/json"},
+        //     body: JSON.stringify({
+        //         username: username
+        //     })
+        // })
+        // .then(response => response.json())
+        // .then(player => {
+        //     // renderPlayerProfile(player) //figure this out
+        //     console.log(player) playerStats()
+        // })
     
 
     // fetchAndLoadPlayer() {
@@ -95,7 +97,7 @@
     
 
 
-// }
+}
 
 
 // Form for username
